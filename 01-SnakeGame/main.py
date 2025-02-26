84f4ad54d71b16e5b56e9b4488f8134197dba720
firatbezir <bezir.1@osu.edu>
2025-02-26 19:38:43 +0300

    Added Snake Game

---
+++ 01-SnakeGame/main.py
@@ -0,0 +1,51 @@
+from turtle import Screen
+from food import Food
+from scoreboard import ScoreBoard
+from snake import Snake
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+# TODO: Create Snake Body
+snake = Snake()
+food = Food()
+scoreboard = ScoreBoard()
+
+
+screen.listen()
+screen.onkey(snake.up, "w")
+screen.onkey(snake.down, "s")
+screen.onkey(snake. left, "a")
+screen.onkey(snake.right, "d")
+
+
+game_is_on = True
+while game_is_on:
+    screen.update() # REFRESH THE SCREEN
+    time.sleep(0.1) # DELAY THE REFRESH
+    snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_the_score()
+
+    # Detect collision with wall.
+    if (snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or
+            snake.head.ycor() < -290):
+        game_is_on = False
+        scoreboard.game_over()
+
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
+
+screen.exitonclick()
---
+++ 01-SnakeGame/scoreboard.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Arial", 12, "normal")
+
+
+class ScoreBoard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.hideturtle()
+        self.penup()
+        self.color("white")
+        self.goto(0, 275)
+        self.update_score()
+
+    def update_score(self):
+        self.clear()
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
+
+    def increase_the_score(self):
+        self.score += 1
+        self.update_score()
