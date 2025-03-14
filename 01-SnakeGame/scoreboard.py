06c2c3a1f22cd13c10a8b83524a08d08d6d521bc
firatbezir <bezir.1@osu.edu>
2025-03-14 20:56:42 +0300

    Score tracking added

--- 01-SnakeGame/scoreboard.py
+++ 01-SnakeGame/scoreboard.py
@@ -1,12 +1,14 @@
 from turtle import Turtle
+
 ALIGNMENT = "center"
 FONT = ("Arial", 12, "normal")
-
 
 class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("./externalfiles/data.txt") as data:
+            self.highscore = int(data.read())
         self.hideturtle()
         self.penup()
         self.color("white")
@@ -15,11 +17,15 @@ class ScoreBoard(Turtle):
 
     def update_score(self):
         self.clear()
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} High Score: {self.highscore}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset_score(self):
+        if self.score > int(self.highscore):
+            self.highscore = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.highscore}")
+        self.score = 0
+        self.update_score()
 
 
     def increase_the_score(self):
